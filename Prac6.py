71f778870a6474c7d92706a870fd26844afae40f
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 10:13:33 +0530

    P6

---
+++ Prac6.py
@@ -0,0 +1,11 @@
+# def maximum(a, b, c):
+  #  list = [a, b, c]
+   # return max(list)
+a=int(input('Enter value of A:'))
+b=int(input('Enter value of B:'))
+c=int(input('Enter value of C:'))
+print(max(a, b, c))
+# print("Maximum Number is ::>",maximum(a, b, c))
+print(type(a))
+
+
